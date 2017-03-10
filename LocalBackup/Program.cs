cb872fdbb4194c68a7cb0df61b18db7ad995fdbd
Aron Parker <hl3mukkel@gmail.com>
2017-03-10 22:56:21 +0100

    Changed project type from ConsoleApplication to WinFormsApplication

--- LocalBackup/Program.cs
+++ LocalBackup/Program.cs
@@ -2,77 +2,20 @@
 using LocalBackup.IO.FileComparers;
 using System;
 using System.IO;
+using System.Windows.Forms;
+using LocalBackup.Forms;
+using LocalBackup.Controller;
 
 namespace LocalBackup
 {
     public static class Program
     {
+        [STAThread]
         public static void Main()
         {
-            var detector = new DirectoryMirrorer();
-            detector.ItemFound += (o, e) =>
-            {
-                Console.WriteLine(e.FileSystemItem);
-            };
-            detector.RunAsync(new DirectoryInfo(@"C:\Users\Aron\Desktop\1"),
-                              new DirectoryInfo(@"C:\Users\Aron\Desktop\2"),
-                              new FileComparer());
-            /*var finder = new DuplicateFinder();
-            var dirs = new List<DirectoryInfo>();
-            dirs.Add(new DirectoryInfo(@"C:\Users\Aron\Desktop\1"));*/
-
-            Console.ReadLine();
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            new BackupController().Run();
         }
-        /*private IFileComparer CreateFileComparer(DirectoryInfo dstDir)
-{
-   if (_fullScan)
-       return new FileComparer();
-
-   var driveFormat = new DriveInfo(dstDir.FullName).DriveFormat;
-
-   switch (driveFormat)
-   {
-       case "FAT32":
-       case "exFAT":
-           return new FATFileComparer();
-       case "NTFS":
-           return new NTFSFileComparer();
-       default:
-           throw new NotSupportedException("Destination uses a file system where quick scan is not supported.");
-   }
-}*/
-
-        /*public class DirectoryError : FileSystemError
-        {
-        private DirectoryInfo _dir;
-
-        public DirectoryError(DirectoryInfo dir, Exception ex) : base(ex)
-        {
-            _dir = dir;
-        }
-
-        public override string Name => "Directory error";
-        public override FileSystemItemType Type => FileSystemItemType.DirectoryError;
-        public override string FileName => _dir.Name;
-        public override string FullPath => _dir.FullName;
-        public DirectoryInfo Directory => _dir;
-        }*/
-
-        /*    public class FileError : FileSystemError
-        {
-        private FileInfo _file;
-
-        public FileError(FileInfo file, Exception ex) : base(ex)
-        {
-            _file = file;
-        }
-
-        public override string Name => "File error";
-        public override FileSystemItemType Type => FileSystemItemType.FileError;
-        public override string FileName => _file.Name;
-        public override string FullPath => _file.FullName;
-        public FileInfo File => _file;
-        }*/
     }
 }
