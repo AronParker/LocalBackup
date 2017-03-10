e155a6d8e3237cc2868eb60e9090a0e5d27828e7
Aron Parker <hl3mukkel@gmail.com>
2017-03-10 22:05:49 +0100

    Added BackupController

---
+++ LocalBackup/Controller/BackupController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LocalBackup.Forms;
+
+namespace LocalBackup.Controller
+{
+    public class BackupController
+    {
+        private MainForm _mainForm;
+        private State _state;
+
+        public BackupController()
+        {
+            _mainForm = new MainForm();
+        }
+        
+        public void Run()
+        {
+            Application.Run(_mainForm);
+        }
+    }
+}
