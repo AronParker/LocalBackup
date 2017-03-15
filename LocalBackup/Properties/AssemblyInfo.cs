0b5cb944c82119b74d76924d4e880a166cee62e8
Aron Parker <hl3mukkel@gmail.com>
2017-03-15 22:36:52 +0100

    Updated AssemblyInfo

--- LocalBackup/Properties/AssemblyInfo.cs
+++ LocalBackup/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Local Backup")]
-[assembly: AssemblyDescription("A tool designed to create backups locally.")]
+[assembly: AssemblyDescription("A tool designed to create backups.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aron Parker")]
 [assembly: AssemblyProduct("Local Backup")]
